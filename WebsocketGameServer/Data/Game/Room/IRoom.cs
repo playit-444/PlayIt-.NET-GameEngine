c7b82e12aec20e83977bcf4377339f466f24a989
Christoffer <42372704+Cp-Itoperators@users.noreply.github.com>
2020-05-22 11:53:57 +0200

    Added RoomManager implementaion

--- WebsocketGameServer/Data/Game/Room/IRoom.cs
+++ WebsocketGameServer/Data/Game/Room/IRoom.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebsocketGameServer.Data.Game.Player;
 
 namespace WebsocketGameServer.Data.Game.Room
 {
     public interface IRoom
     {
+        public string RoomID { get; }
+        public HashSet<IPlayer> Players { get; }
     }
 }
